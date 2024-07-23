c7590062c3aa663c362a16e083a7ec3050e52b7c
watermelon1024 <watermelonman1024@gmail.com>
2024-07-23 23:09:43 +0800

    feat: default server host

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 from src.app import app
 
-load_dotenv()
+load_dotenv(override=True)
 
 if __name__ == "__main__":
     import uvicorn
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     try:
         uvicorn.run(
             app,
-            host=os.getenv("SERVER_HOST") or "127.0.0.1",
+            host=os.getenv("SERVER_HOST") or "0.0.0.0",
             port=int(os.getenv("SERVER_PORT") or 8000),
         )
     except KeyboardInterrupt:
